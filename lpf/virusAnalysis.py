85c8092c0398afa4b774461443b52ce0a466b08f
MBHallgren <s153002@student.dtu.dk>
2023-06-19 15:24:38 +0200

    push3

--- lpf/virusAnalysis.py
+++ lpf/virusAnalysis.py
@@ -26,40 +26,58 @@ def virus_analysis_pipeline(virus_parser):
                                     .format(virus_parser.data.entry_id, 'virus'), virus_parser.data.sql_db)
     sqlCommands.sql_update_status_table('Virus alignment', virus_parser.data.sample_name, '2',
                                         virus_parser.data.entry_id, virus_parser.data.sql_db)
+    try:
+        KMARunner(virus_parser.data.input_path,
+                  virus_parser.data.target_dir + "/virus_alignment",
+                  virus_parser.data.virus_db,
+                  "-ont -ca -1t1 -mem_mode").run()
+    except Exception as e:
+        virus_parser.logger.error("Error in virus alignment")
+        virus_parser.logger.error("KMA failed: {}".format(e))
 
-    KMARunner(virus_parser.data.input_path,
-              virus_parser.data.target_dir + "/virus_alignment",
-              virus_parser.data.virus_db,
-              "-ont -ca -1t1 -mem_mode").run()
+
+
 
     #Consider identity and perhaps assemble if its bad:
 
     sqlCommands.sql_update_status_table('CDD alignment', virus_parser.data.sample_name, '3',
                                         virus_parser.data.entry_id, virus_parser.data.sql_db)
-
-    KMARunner(virus_parser.data.input_path,
-              virus_parser.data.target_dir + "/cdd_alignment",
-              virus_parser.data.cdd_db,
-              "-ont -ca -1t1 -mem_mode").run()
+    try:
+        KMARunner(virus_parser.data.input_path,
+                  virus_parser.data.target_dir + "/cdd_alignment",
+                  virus_parser.data.cdd_db,
+                  "-ont -ca -1t1 -mem_mode").run()
+    except Exception as e:
+        virus_parser.logger.error("Error in CDD alignment")
+        virus_parser.logger.error("KMA failed: {}".format(e))
 
     sqlCommands.sql_update_status_table('Prokka annotation', virus_parser.data.sample_name, '2',
                                         virus_parser.data.entry_id, virus_parser.data.sql_db)
-
-    prokkaRunner(virus_parser.data.sample_name,
-                 virus_parser.data.target_dir + "/virus_alignment.fsa",
-                 virus_parser.data.entry_id,
-                 virus_parser.data.target_dir).run()
+    try:
+        prokkaRunner(virus_parser.data.sample_name,
+                     virus_parser.data.target_dir + "/virus_alignment.fsa",
+                     virus_parser.data.entry_id,
+                     virus_parser.data.target_dir).run()
+    except Exception as e:
+        virus_parser.logger.error("Error in prokka annotation")
+        virus_parser.logger.error("Prokka failed: {}".format(e))
 
     #Phylogenetic analysis
     #Pathogenicy prediction
-
-    virus_parser.parse_virus_results()
+    try:
+        virus_parser.parse_virus_results()
+    except Exception as e:
+        virus_parser.logger.error("Error in parsing virus results")
+        virus_parser.logger.error("Parsing failed: {}".format(e))
 
     sqlCommands.sql_update_status_table('Compiling PDF', virus_parser.data.sample_name, '9', virus_parser.data.entry_id, virus_parser.data.sql_db)
 
     #local_phylogeny_from_input_and_database(virus_parser.data.input_path, virus_parser.data.virus_db, virus_parser.data.reference_header_text.split(':')[1], virus_parser.data.target_dir)
-
-    pdfReport.compile_virus_report(virus_parser)
+    try:
+        pdfReport.compile_virus_report(virus_parser)
+    except Exception as e:
+        virus_parser.logger.error("Error in compiling PDF")
+        virus_parser.logger.error("PDF compilation failed: {}".format(e))
 
     sqlCommands.sql_update_status_table('Analysis completed', virus_parser.data.sample_name, '10', virus_parser.data.entry_id, virus_parser.data.sql_db)
 
